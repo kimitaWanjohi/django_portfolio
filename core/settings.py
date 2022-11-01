fd1517e41079897d67d8a5cc3437e61fd06ed7a2
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-11-01 12:37:40 +0300

    django summer note and admin

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'portfolio.apps.PortfolioConfig',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Eductation, Experience, Project, Blogs
 
-admin.site.register(Eductation)
-admin.site.register(Experience)
-admin.site.register(Project)
-admin.site.register(Blogs)
+@admin.register(Eductation)
+class EductationAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ('name', 'school', 'start_date', 'end_date')
+
+@admin.register(Experience)
+class ExperienceAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ('name', 'company', 'start_date', 'end_date')
+
+@admin.register(Project)
+class ProjectAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)
+    list_display = ('name', 'live_link', 'source_link', 'start_date', 'end_date')
+
+@admin.register(Blogs)
+class BlogsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('brief_description',)
+    list_display = ('title', 'blog_link', 'blog_image_link')
+
 
