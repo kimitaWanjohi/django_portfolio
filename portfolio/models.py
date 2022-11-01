18abb3e2c7ed2112915021fcfa09c4fb3657790a
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-11-01 21:20:11 +0300

    reusme

--- portfolio/models.py
+++ portfolio/models.py
@@ -23,9 +23,9 @@ class Project(models.Model):
     screenshot = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=255)
     description = models.TextField()
+    technologies = models.CharField(max_length=255)
     live_link = models.URLField(max_length=200)
     source_link = models.URLField(max_length=200)
-    duration = models.CharField(max_length=50)
 
     
     def __str__(self):
