32e47d0748c38dd143a6a3e36856ce33416530b8
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-11-01 12:03:29 +0300

    done with index.html and models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Eductation(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Experience(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    screenshot = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    live_link = models.URLField(max_length=200)
+    source_link = models.URLField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+
+class Blogs(models.Model):
+    title = models.CharField(max_length=255)
+    blog_image_link = models.URLField(max_length=200)
+    blog_link = models.URLField(max_length=200)
+    brief_description = models.TextField()
+
+    def __str__(self):
+        return self.title
