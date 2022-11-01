3f63b1cd02f48cd0e51f12669ef007251acd04ba
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-11-01 13:00:59 +0300

    views and experience

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import Eductation, Experience, Project, Blogs
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'educations': Eductation.objects.all(),
+        'experiences': Experience.objects.all(),
+        'projects': Project.objects.all(),
+        'blogs': Blogs.objects.all(),
+    }
+    return render(request, 'index.html', context)
