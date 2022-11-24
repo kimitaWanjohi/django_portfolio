2d469018538386fae676ec9d5717c7259e65cba2
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-11-24 14:56:18 +0300

    added white noise to dependancies

--- core/settings.py
+++ core/settings.py
@@ -124,11 +124,11 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
